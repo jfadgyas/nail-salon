630da58fc2b2d9a5128223c6020ad37db532c468
jfadgyas <djdeville72@yahoo.com>
2021-05-21 10:13:37 +0200

    sliding gallery

--- script/gallery.js
+++ script/gallery.js
@@ -44,34 +44,40 @@ const scroll = (nextImage, nextPos) => {
 
 // Swiper functions: start, move, end
 const swipeStart = e => {
+    e.preventDefault()
     console.log('start')
     console.log(e)
-    swipeStartPos = e.type.includes('touch') ? e.touches[0].clientX : e.pageX
+    // swipeStartPos = e.type.includes('touch') ? e.touches[0].clientX : e.pageX
 }
 
 const swipeEnd = e => {
+    e.preventDefault()
+    console.log('end')
     console.log(e)
-    const swipeEndPos = e.type.includes('touch') ? e.touches[0].clientX : e.pageX
-    if (swipeEndPos < swipeStartPos - 100){
-        scroll('nextElementSibling', -1)
-    }
-    if (swipeEndPos > swipeStartPos + 100){
-        scroll('previousElementSibling', 1)
-    }
+    // const swipeEndPos = e.type.includes('touch') ? e.touches[0].clientX : e.pageX
+    // if (swipeEndPos < swipeStartPos - 100){
+    //     scroll('nextElementSibling', -1)
+    // }
+    // if (swipeEndPos > swipeStartPos + 100){
+    //     scroll('previousElementSibling', 1)
+    // }
 }
 
-// const swipe = () => {
-//     console.log('move')
-// }
+const swipe = e => {
+    e.preventDefault()
+    console.log('move')
+    // console.log(e)
+    gallery.style.background = 'red'
+}
 
 // Listeners
 document.querySelector('#left').addEventListener('click', () => scroll('nextElementSibling', -1))
 document.querySelector('#right').addEventListener('click', () => scroll('previousElementSibling', 1))
 
-gallery.addEventListener('mousedown', swipeStart)
-gallery.addEventListener('mouseup', swipeEnd)
-// gallery.addEventListener('mousemove', () => swipe()) // kellez?
+// gallery.addEventListener('mousedown', swipeStart)
+// gallery.addEventListener('mouseup', swipeEnd)
 gallery.addEventListener('touchstart', swipeStart)
 gallery.addEventListener('touchend', swipeEnd)
+gallery.addEventListener('touchmove', swipe)
 
 getImages()
